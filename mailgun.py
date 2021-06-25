45c1f33cadbe9547880503759e3901b7d655f90a
marceloganin <marceloganin@gmail.com>
2021-06-24 22:04:52 -0300

    change From to sender

--- mailgun.py
+++ mailgun.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import argparse
 
 
-def send_html_message(domain, From, to, cc, bcc, subject, body, attach_files, html ):
+def send_html_message(domain, sender, to, cc, bcc, subject, body, attach_files, html ):
     template = None
     files=[]
     
@@ -20,7 +20,7 @@ def send_html_message(domain, From, to, cc, bcc, subject, body, attach_files, ht
         "https://api.mailgun.net/v3/"+domain+"/messages",
         auth=("api", "  yourKeys"),
         files=files,
-        data={"from": From,
+        data={"from": sender,
               "to": to, 
               "cc": cc,
               "bcc": bcc,             
@@ -35,7 +35,7 @@ if __name__ == "__main__":
                                      epilog="Example: python3 mailgun.py -d example.com -f frommail@example.com -t examplemail@gmail.com -s 'subject' -m 'text body'")
 
     parser.add_argument('-d', '--domain', help='-d example.com', type=str)
-    parser.add_argument('-f', '--From', help='-f address', type=str)
+    parser.add_argument('-f', '--sender', help='-f address', type=str)
     parser.add_argument('-t', '--to', help='-t address', type=str, nargs="+")
     parser.add_argument('-cc', '--cc', help='-cc address', type=str, nargs="+")
     parser.add_argument('-bcc', '--bcc', help='-bcc address', type=str, nargs="+")
@@ -45,5 +45,5 @@ if __name__ == "__main__":
     parser.add_argument('-o', '--html', help='html content', type=str, default=None)
     args = parser.parse_args()
     
-    response = send_html_message(args.domain, args.From, args.to, args.cc, args.bcc, args.subject, args.body, args.attach, args.html)
+    response = send_html_message(args.domain, args.sender, args.to, args.cc, args.bcc, args.subject, args.body, args.attach, args.html)
     print (response.text )
